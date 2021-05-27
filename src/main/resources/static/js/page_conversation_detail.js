b9910e6d8ad3044b5f23452f6f1296a07e418b77
Yegabebar <m.barbegayet@gmail.com>
2021-05-27 20:06:30 +0200

    Live reload feature completed

--- src/main/resources/static/js/page_conversation_detail.js
+++ src/main/resources/static/js/page_conversation_detail.js
@@ -1,3 +1,8 @@
+$(document).ready(function() {
+
+    setInterval("refreshMessages();", 5000)
+
+});
 const contentInput = document.getElementById("content");
 
 contentInput.addEventListener("keyup", function (event) {
@@ -6,3 +11,33 @@ contentInput.addEventListener("keyup", function (event) {
         document.getElementById("sendMessage").click();
     }
 });
+
+function refreshMessages(){
+    url = window.location.pathname
+    var id = url.substring(url.lastIndexOf('/') + 1);
+
+    $.ajax({
+      type: "GET",
+      url: `/conversations/${id}`,
+      dataType: "text",
+      success: function(result){
+        dom = stringToHTML(result);
+        console.log("DOM: ",dom)
+        messages = dom.children[0].children[0].children[1].children[1].children[0].children[0].children[1].children[1].children[0]
+        for(item of messages['children']){
+            // Condition if message not already in DOM
+            console.log(item)
+
+            //document.getElementById("messages")[0].innerHTML += item.outerHTML
+
+        }
+        document.getElementById("messages").innerHTML = messages.outerHTML
+      }
+  });
+
+}
+  var stringToHTML = function (str) {
+  	var parser = new DOMParser();
+  	var doc = parser.parseFromString(str, 'text/html');
+  	return doc.body;
+  };
